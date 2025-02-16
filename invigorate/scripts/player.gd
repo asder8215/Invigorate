65898196594519d16b01ccd544d064d541ba5a7b
Mahdi Ali-Raihan <65004219+asder8215@users.noreply.github.com>
2025-02-16 02:51:58 -0500

    Added dash movement, added doors, camera is fixed so that it shows the room (tho need to figure out how the window zoom out will accommodate showing just the room; need more room designs

--- invigorate/scripts/player.gd
+++ invigorate/scripts/player.gd
@@ -1,11 +1,33 @@
 extends CharacterBody2D
 
 
-@export var speed = 150
+@export var SPEED = 150
+const DASH = 300
+#var timer = Timer.new()
+var tween : Tween
+var dash_vel = 0.0
+var can_dash = true
+
+func dash_cooldown():
+	var timer = get_tree().create_timer(0.75)
+	timer.timeout.connect(dash_timeout)
+
+func dash_timeout():
+	can_dash = true
 
 func read_input():
+	if Input.is_action_just_pressed("Roll") && can_dash:
+		dash_vel = DASH
+		if tween:
+			tween.stop()
+		tween = create_tween()
+		tween.tween_property(self, "dash_vel", 0, 0.3).set_ease(Tween.EASE_OUT)
+		can_dash = false
+		dash_cooldown()
+		
+	
 	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_dir * speed
+	velocity = input_dir * (SPEED + dash_vel)
 	
 func _physics_process(delta):
 	read_input()
